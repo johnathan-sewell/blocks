4c5b1e20fcce693d30f4ce092937663161b6f4d9
Mads Stoumann <stoumann@gmail.com>
2016-04-06 16:17:33 +0200

    Select

--- js/playvideo.js
+++ js/playvideo.js
@@ -7,7 +7,9 @@
 	function playVideo() {
 		var oVideo = this.parentNode.querySelector(".video-content");
 		if (oVideo && oVideo.src) {
-			oVideo.src += "&autoplay=1";
+			if (oVideo.src.indexOf("ooyala") != -1) oVideo.src += "&options[autoplay]=true";
+			if (oVideo.src.indexOf("vimeo") != -1) oVideo.src += "?autoplay=1";
+			if (oVideo.src.indexOf("youtube") != -1) oVideo.src += "&autoplay=1";
 			this.classList.add("hide");
 		}
 	}
---
+++ js/select.js
@@ -0,0 +1,24 @@
+﻿(function() {
+	var aSelect = document.querySelectorAll(".select");
+	
+	aSelect.forEach(function(oSelect, nIdx) {
+		var
+		oChecked = oSelect.querySelector(".select-panel .state:checked");
+		if (oChecked) oChecked = oSelect.querySelector("[for=\"" + oChecked.id + "\"]");
+		if (oChecked) setSelected(oSelect, oChecked.innerHTML);
+		
+		oSelect.addEventListener("click", function(e) {
+			if (e.target.classList.contains("select-option")) setSelected(oSelect, e.target.innerText);
+		}, false);
+	});
+	
+	function setSelected(oSelect, sOptionText) {
+		var 
+		oCheck = oSelect.querySelector("[type=\"checkbox\"]"),
+		oOption = oSelect.querySelector(".select-label-option");
+		if (oCheck && oOption) {
+			oOption.innerText = sOptionText;
+			oCheck.checked = false;
+		}	
+	}
+})();
